42ffc6bfee5934f556f64ca5f13109cb6345b7d0
Tshumelo Matshusa <tshumelomatshusa@gmail.com>
2023-10-03 14:34:46 -0700

    task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,7 +3,7 @@ def uppercase(str):
     uppercase = ""
     for letter in str:
         if ord(letter) > 96 and ord(letter) < 123:
-            uppercase = (ord(letter) + 32)
+            uppercase = (ord(letter) - 32)
             print("{}".format(chr(uppercase)), end="")
         else:
             str1 = letter
