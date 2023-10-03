cccf616a3b5fedc9ae9472a635563a8b4cf15940
Tshumelo Matshusa <tshumelomatshusa@gmail.com>
2023-10-03 13:02:29 -0700

    update

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 100):
-    if i < 10:
-        print("{}".format(f"{i:02}"), end= ", ")
-    elif i == 99:
-        print("99")
+    if i == 99:
+        print("{}".format(i))
     else:
-        print("{}".format(i), end=", ")
+        print("{:02}".format(i), end= ", ")
