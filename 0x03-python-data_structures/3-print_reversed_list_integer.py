bd5c9962b27c74a578f667ae5d8d9796ec26954e
Tshumelo Matshusa <tshumelomatshusa@gmail.com>
2023-10-07 01:29:39 -0700

    program that prints all integers of a list, in reverse

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+    """Print list of integers in reverse"""
+    length = len(my_list)
+    for i in range(length):
+        length -= 1
+        print("{}".format(my_list[length]))
