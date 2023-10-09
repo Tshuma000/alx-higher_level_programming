66deb6f2f04224b6ba37a4080b6e693353f0e333
Tshumelo Matshusa <tshumelomatshusa@gmail.com>
2023-10-09 08:51:33 -0700

    program that returns length and first character of a string

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,7 +3,7 @@
 def multiple_returns(sentence):
     """Returns legth and first character of string"""
 
-    if len(sentence) == 0:
+    if sentence == "":
         return (0, None)
     return (len(sentence), sentence[0])
     
