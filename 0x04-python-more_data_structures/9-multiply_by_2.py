34591a36f8615d4ae9fe8a47a02c036e70393ab6
Tshumelo Matshusa <tshumelomatshusa@gmail.com>
2023-10-11 09:56:18 -0700

    returns a new dict with all values multiplied

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -3,8 +3,8 @@
 def multiply_by_2(a_dictionary):
     """Retruns a new dict with all values * 2"""
     new_dict = a_dictionary.copy()
-    list_keys = list(new_dir.keys())
+    list_keys = list(new_dict.keys())
 
     for i in list_keys:
-        new_dir[i] *= 2
-    return (new_dir)
+        new_dict[i] *= 2
+    return (new_dict)
