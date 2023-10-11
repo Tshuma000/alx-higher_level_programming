6f8cb044fd149b287c74ae6ada0d2a127d0183d2
Tshumelo Matshusa <tshumelomatshusa@gmail.com>
2023-10-11 09:17:06 -0700

    program that adds unique integers in a list

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def uniq_add(my_list=[]):
+    """Adds all unique integers in a list"""
+    new_set = set(my_list)
+    sum = 0
+
+    for i in new_set:
+        sum += i
+    return (sum)
