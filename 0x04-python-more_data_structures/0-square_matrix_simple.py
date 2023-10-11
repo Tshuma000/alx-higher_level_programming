d52851426d44dc5ac3509013a24cf560047edf3b
Tshumelo Matshusa <tshumelomatshusa@gmail.com>
2023-10-11 09:06:31 -0700

    program that computes square value of a matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -6,4 +6,4 @@ def square_matrix_simple(matrix=[]):
 
     for i in range(len(matrix)):
         new_matrix[i] = list(map(lambda x: x**2, matrix[i]))
-    return (new_matriix)
+    return (new_matrix)
