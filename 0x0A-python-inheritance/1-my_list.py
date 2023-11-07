955b8b5a2afa6387472fb198341583c220386b65
Tshumelo Matshusa <tshumelomatshusa@gmail.com>
2023-11-07 10:26:54 -0800

    whats going on

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Defines an inherited list class Mylist."""
 
-class Mylist(list):
+class MyList(list):
     """myLists inherits from list"""
     def print_sorted(self):
         """prints a list in ascending order"""
