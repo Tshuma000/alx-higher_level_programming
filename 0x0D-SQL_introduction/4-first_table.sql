2f7477e5152706c30c9deb4f6af505e3bd6529eb
Matshusa Tshumelo <tshumelomatshusa@gmail.com>
2023-12-13 09:52:44 -0800

    fix code for task 4

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,3 +1,2 @@
 -- Creates a table called 'first_table'.
-
-CREATE TABLE if NOT EXISTS 'first_table' ('id' INT, 'name' VARCHAR(256));
+CREATE TABLE IF NOT EXISTS 'first_table' ('id' INT, 'name' VARCHAR(256));
